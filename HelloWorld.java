d19d28035859efe4d156d19997892fd49862a3be
R.GOPALAKRISHNAN <42864066+GOPALAKRISHNAN-R@users.noreply.github.com>
2019-05-09 22:08:18 +0530

    Add files via upload

---
+++ HelloWorld.java
@@ -0,0 +1,19 @@
+//: object/HelloWorld.java
+import java.util.*;
+/** The first Java example program.
+* Displays a string.
+* @author R.GOPALAKRISHNAN
+* @author www.rgopalakrishnanmca.simplesite.com
+**/
+public class HelloWorld {
+/** Entry point to class & application.
+* @param args array of string arguments
+* @throws exceptions No exceptions thrown
+*/
+public static void main(String[] args) 
+{
+	System.out.println("Hello, World");
+}
+} /* Output: (100% match)
+Hello, World
+*///:~
